32d8a2837d9bb6f1299abf39a07b96bfcde1edf9
James Home <jh@jameshome.com>
2026-02-21 21:47:07 -0800

    chore: update workflow

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,21 @@
+// content.config.mjs
+import {
+  ObsidianDocumentSchema,
+  ObsidianMdLoader,
+} from "astro-loader-obsidian";
 import { defineCollection } from "astro:content";
-import { ObsidianMdLoader } from "astro-loader-obsidian";
 
-const notes = defineCollection({
-  loader: ObsidianMdLoader({
-    base: "src/content/vagabondage",
-    url: "notes",
+export const collections = {
+  documents: defineCollection({
+    loader: ObsidianMdLoader({
+      base: "src/content/vagabondage/",
+      url: "notes",
+    }),
+    schema: ({ image }) =>
+      ObsidianDocumentSchema.extend({
+        image: image().optional(),
+        // or
+        cover: image().optional(),
+      }),
   }),
-});
-
-export const collections = { notes };
+};
